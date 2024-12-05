14895c80ca454f88d745ad604b4af3f8c54a7fef
Zakaal <mochammadzakaro24@gmail.com>
2024-12-05 13:51:38 +0700

    W11: Soal 12

--- Pertemuan11/books/lib/main.dart
+++ Pertemuan11/books/lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       //home: const FuturePage(),
-      home: const NavigationDialogScreen(),
+      home: const LocationScreen(),
     );
   }
 }
